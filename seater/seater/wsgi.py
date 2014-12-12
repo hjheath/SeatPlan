bf016e4e8d089aab5ea2f35584f124ebba8e4279
heathy <henryjheath@gmail.com>
2014-12-12 18:31:46 +0000

    Initialise django app.

---
+++ seater/seater/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for seater project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seater.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
